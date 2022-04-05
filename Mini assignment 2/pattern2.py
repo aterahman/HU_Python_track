dcaa73d5f0715c84cbeef65fead3ffff7220d311
Rahman <aterahman@deloitte.com>
2022-04-05 16:03:11 +0530

    Part b of the mini assignment 2

---
+++ Mini assignment 2/pattern2.py
@@ -0,0 +1,20 @@
+class q2b:
+
+
+    for i in range (0, 9,2):
+        for j in range (1, 10,2):
+            if (j < (9 - i)):
+                    print(" ",end='')
+            else:
+                    print("*",end='')
+
+        print()
+
+
+    for i in range(1,10,2):
+        for j in range(9,0,-2):
+            if (j > (9 - i)):
+                print(' ',end='')
+            else:
+                print("*",end='')
+        print()
