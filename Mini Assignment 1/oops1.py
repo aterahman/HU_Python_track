c77520661677041709070af631f0c7ccb7b89fd5
Rahman <aterahman@deloitte.com>
2022-04-05 12:27:46 +0530

    mini assignment 1

---
+++ Mini Assignment 1/oops1.py
@@ -0,0 +1,15 @@
+#defining a class named StringClass
+class StringClass:
+
+    #creating a constructor to take input of string
+    def __init__(self):
+        self.str = input("Enter the desired string")
+
+
+    def length(self):
+        return len()
+
+
+ob1 = StringClass();
+ob1.length();
+print(ob1.str)
