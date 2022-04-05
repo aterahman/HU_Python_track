c96e66779b2f9d319a2f1c8ada7150afaf82a41d
Rahman <aterahman@deloitte.com>
2022-04-05 16:06:52 +0530

    made changes to loop in Question 2b

--- Mini assignment 2/pattern2.py
+++ Mini assignment 2/pattern2.py
@@ -2,7 +2,7 @@ class q2b:
 
 
     for i in range (0, 9,2):
-        for j in range (1, 10,2):
+        for j in range (1, 10):
             if (j < (9 - i)):
                     print(" ",end='')
             else:
